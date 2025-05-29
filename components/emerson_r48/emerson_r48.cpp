de322b9dd6c0fda782ced749134e29dc35affda4
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-05-29 23:11:57 +0200

    Update emerson_r48.cpp

--- components/emerson_r48/emerson_r48.cpp
+++ components/emerson_r48/emerson_r48.cpp
@@ -80,8 +80,13 @@ void EmersonR48Component::setup() {
 void EmersonR48Component::update() {
   static uint8_t cnt = 0;
   cnt++;
+    if (cnt == 1) {
+    ESP_LOGD(TAG, "Requesting all sensors");
+    std::vector<uint8_t> data = {0x00, 0xF0, 0x00, 0x80, 0x46, 0xA5, 0x34, 0x00};
+    this->canbus->send_data(CAN_ID_REQUEST, true, data);
+  }
 
-  if (cnt == 1) {
+/*  if (cnt == 1) {
     ESP_LOGD(TAG, "Requesting output voltage message");
     std::vector<uint8_t> data = {0x01, 0xF0, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00};
     this->canbus->send_data(CAN_ID_REQUEST, true, data);
@@ -141,7 +146,17 @@ void EmersonR48Component::update() {
     if (millis() - this->lastCtlSent_ > 10000) {
 
       this->lastCtlSent_ = millis();
-    }
+    }*/
+ if (cnt == 2) { 
+    cnt = 0; 
+    // send control every 10 seconds
+    uint8_t msgv = this->dcOff_ << 7 | this->fanFull_ << 4 | this->flashLed_ << 3 | this->acOff_ << 2 | 1;
+    this->set_control(msgv);
+
+    if (millis() - this->lastCtlSent_ > 10000) {
+
+      this->lastCtlSent_ = millis();
+  
   }
 
 
