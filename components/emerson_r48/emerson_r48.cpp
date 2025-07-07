eb62c41355b66094d86a03dde08c9a78bf4699e4
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-07-07 13:52:19 +0200

    Update emerson_r48.cpp

--- components/emerson_r48/emerson_r48.cpp
+++ components/emerson_r48/emerson_r48.cpp
@@ -88,11 +88,11 @@ void EmersonR48Component::setup() {
 void EmersonR48Component::update() {
   static uint8_t cnt = 0;
   cnt++;
-/*    
+/*    */
     ESP_LOGD(TAG, "Requesting all sensors");
     std::vector<uint8_t> data = {0x00, 0xF0, 0x00, 0x80, 0x46, 0xA5, 0x34, 0x00};
     this->canbus->send_data(CAN_ID_REQUEST, true, data);
-*/
+
   if (cnt == 1) {
     ESP_LOGD(TAG, "Requesting output voltage message");
     std::vector<uint8_t> data = {0x01, 0xF0, 0x00, EMR48_DATA_OUTPUT_V, 0x00, 0x00, 0x00, 0x00};
@@ -187,7 +187,7 @@ void EmersonR48Component::update() {
 
 
   // no new value for 5* intervall -> set sensors to NAN)
-  if (millis() - lastUpdate_ > this->update_interval_ * 200 && cnt == 0) {
+  if (millis() - lastUpdate_ > this->update_interval_ * 5 && cnt == 0) {
     this->publish_sensor_state_(this->input_power_sensor_, NAN);
     this->publish_sensor_state_(this->input_voltage_sensor_, NAN);
     this->publish_sensor_state_(this->input_current_sensor_, NAN);
