ee274fd19714997c4606325f83a69a1459f5e190
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-07-07 14:12:56 +0200

    Update emerson_r48.cpp

--- components/emerson_r48/emerson_r48.cpp
+++ components/emerson_r48/emerson_r48.cpp
@@ -88,11 +88,11 @@ void EmersonR48Component::setup() {
 void EmersonR48Component::update() {
   static uint8_t cnt = 0;
   cnt++;
-/*    */
+/*    
     ESP_LOGD(TAG, "Requesting all sensors");
     std::vector<uint8_t> data = {0x00, 0xF0, 0x00, 0x80, 0x46, 0xA5, 0x34, 0x00};
     this->canbus->send_data(CAN_ID_REQUEST, true, data);
-
+*/
   if (cnt == 1) {
     ESP_LOGD(TAG, "Requesting output voltage message");
     std::vector<uint8_t> data = {0x01, 0xF0, 0x00, EMR48_DATA_OUTPUT_V, 0x00, 0x00, 0x00, 0x00};
@@ -163,11 +163,11 @@ void EmersonR48Component::update() {
   if (cnt == 6) { 
     cnt = 0; 
     // send control every 10 seconds
-    uint8_t msgv = this->dcOff_ << 7 | this->fanFull_ << 4 | this->flashLed_ << 3 | this->acOff_ << 2 | 1;
-    this->set_control(msgv);
+    
 
     if (millis() - this->lastCtlSent_ > 10000) {
-
+      uint8_t msgv = this->dcOff_ << 7 | this->fanFull_ << 4 | this->flashLed_ << 3 | this->acOff_ << 2 | 1;
+      this->set_control(msgv);
       this->lastCtlSent_ = millis();
     }
     
