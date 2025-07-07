ee2acd33072f7cf7fe07ffc44ae12a160f60b9f2
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-07-07 14:15:39 +0200

    Update emerson_r48.cpp

--- components/emerson_r48/emerson_r48.cpp
+++ components/emerson_r48/emerson_r48.cpp
@@ -163,11 +163,11 @@ void EmersonR48Component::update() {
   if (cnt == 6) { 
     cnt = 0; 
     // send control every 10 seconds
-    
+          uint8_t msgv = this->dcOff_ << 7 | this->fanFull_ << 4 | this->flashLed_ << 3 | this->acOff_ << 2 | 1;
+      this->set_control(msgv);
 
     if (millis() - this->lastCtlSent_ > 10000) {
-      uint8_t msgv = this->dcOff_ << 7 | this->fanFull_ << 4 | this->flashLed_ << 3 | this->acOff_ << 2 | 1;
-      this->set_control(msgv);
+
       this->lastCtlSent_ = millis();
     }
     
