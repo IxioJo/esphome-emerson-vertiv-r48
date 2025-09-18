9e7419c677317394e80e141fc3ccf28861f904a1
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-09-18 10:50:20 +0200

    Update __init__.py

--- components/emerson_r48/button/__init__.py
+++ components/emerson_r48/button/__init__.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ENTITY_CATEGORY, CONF_ID
-
+from esphome.const import CONF_ENTITY_CATEGORY, ENTITY_CATEGORY_CONFIG, CONF_ID
 
 from .. import EmersonR48Component, emerson_r48_ns, CONF_EMERSON_R48_ID
 
@@ -16,18 +15,19 @@ CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(CONF_EMERSON_R48_ID): cv.use_id(EmersonR48Component),
-            cv.Optional(CONF_SET_OFFLINE_VALUES): button.button_schema(EmersonR48Button).extend(
+            cv.Optional(CONF_SET_OFFLINE_VALUES): button.BUTTON_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(EmersonR48Button),
                     cv.Optional(
-                        CONF_ENTITY_CATEGORY, default=EntityCategory.CONFIG
+                        CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
                     ): cv.entity_category,
-                    cv.Required("name"): cv.string_strict,
+                    cv.Required('name'): cv.string_strict
                 }
             ),
         }
     ).extend(cv.COMPONENT_SCHEMA)
 )
+
 
 async def to_code(config):
     hub = await cg.get_variable(config[CONF_EMERSON_R48_ID])
@@ -35,5 +35,8 @@ async def to_code(config):
         conf = config[CONF_SET_OFFLINE_VALUES]
         var = cg.new_Pvariable(conf[CONF_ID])
         await cg.register_component(var, conf)
-        await button.register_button(var, conf)
+        await button.register_button(
+            var,
+            conf,
+        )
         cg.add(var.set_parent(hub))
